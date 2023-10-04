d5d0b43babad577b11240d7b8363002bf9a1a0e0
adamwagh512 <106627977+adamwagh512@users.noreply.github.com>
2023-10-04 15:47:41 -0500

    added again

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import navbar from '@/components/navbar'
 
 export default function Home() {
   return (
       <div>
-  
+        <navbar />
+        <h1>Hello</h1>
+        <button>damn it</button>
       </div>
   )
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
 import React from 'react'
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'></div>
+        <Image src="" alt='/'/>
+    </div>
   )
 }
 
