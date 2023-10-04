1ba5d2b397aa581a07ac19e1c7ebcd0fe11af05b
adamwagh512 <106627977+adamwagh512@users.noreply.github.com>
2023-10-04 16:35:11 -0500

    added logo

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Image from 'next/image'
-import navbar from '@/components/navbar'
+import Navbar from '@/components/navbar'
 
 export default function Home() {
   return (
       <div>
-        <navbar />
-        <h1>Hello</h1>
-        <button>damn it</button>
+        <Navbar />
       </div>
   )
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,13 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'></div>
-        <Image src="" alt='/'/>
+    <div className='fixed w-full h-20 shadow-xl z-[100] bg-[#4391DA]'>
+        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16 text-[#ff0303] text-xl font-bold'>
+        <div>
+            <ul>
+                <Link href='/'>Home </Link>
+            </ul>
+        </div>
+        </div>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
